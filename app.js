ce7d25e36dfe6494acb7e33c14a0c3f93f872c7f
yvis trindade <yvis.darsouls@gmail.com>
2024-11-07 22:16:10 -0300

    upload files

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 //variaveis de apoio logico
-let chave = false
+let chave = false // delisgado
+let lampada = true // ligado
 
 function quebrar() {
     document.getElementById('lamp').src = "img/broken.jpg"
@@ -15,13 +16,17 @@ function quebrar() {
     let som = new Audio()
     som.src = "sound/glassbreaking.wav"
     som.play()
+    // apoio a logica para o js indentificar a lampada quebrada
 }
 function onoff() {
-    if (chave === false){
+        if (chave === false && lampada === true){
         document.getElementById('interruptor').src = "img/swon.png"
-    chave = true //o js agora sabe que a chave esta ligada
-}else{
-    document.getElementById('interruptor').src = "img/swoff.png"
-    chave = false
-}
+        chave = true //o js agora sabe que a chave esta ligada
+        document.getElementById('lamp').src = "img/on.jpg"
+        //acender
+    }else {
+        document.getElementById('interruptor').src = "img/swoff.png"
+        chave = false
+        document.getElementById('lamp').src = "img/off.jpg"
+    }
 }
