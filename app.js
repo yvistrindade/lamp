1857c4dc4b0603c4a0f19ee14e7c6e5136a95e2a
yvis trindade <yvis.darsouls@gmail.com>
2024-11-07 21:54:45 -0300

    upload files

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 /**
- * Simples simuladoar de uma lampada
- * @author yvis trindade
+ * Simples simulador de uma lampada
+ * @author João Victor
  */
+
+//variaveis de apoio logico
+let chave = false
 
 function quebrar() {
     document.getElementById('lamp').src = "img/broken.jpg"
-    //reproduzindo um arquivo de acordo
+    //reproduzindo um arquivo de audio do JS
     //passo 1: copiar o arquivo de audio para o projeto
-    //passo2: usar a classe audio(biblioteca interna do jS)
+    //passo 2: Usar a classe Audio(biblioteca interna do Js)
+
     let som = new Audio()
     som.src = "sound/glassbreaking.wav"
     som.play()
+}
+function onoff() {
+    if (chave === false){
+        document.getElementById('interruptor').src = "img/swon.png"
+    chave = true //o js agora sabe que a chave esta ligada
+}else{
+    document.getElementById('interruptor').src = "img/swoff.png"
+    chave = false
+}
 }
